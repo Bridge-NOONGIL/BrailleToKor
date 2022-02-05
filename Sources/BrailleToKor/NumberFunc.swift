f0fcb2fe73aae86613f6f0224d09e859f0ec353c
Yujin Yang <70887135+uujinn@users.noreply.github.com>
2022-02-05 14:46:38 +0900

    숫자 사이 '-' 부호 처리

--- Sources/BrailleToKor/NumberFunc.swift
+++ Sources/BrailleToKor/NumberFunc.swift
@@ -12,7 +12,7 @@ let number_braille = "⠼"
 let number_braille_dict = ["⠚": "0", "⠁": "1", "⠃": "2", "⠉": "3", "⠙": "4", "⠑": "5", "⠋": "6", "⠛": "7", "⠓": "8", "⠊": "9"]
 
 let number_punctuation_invalid = ["~", " ", "⠀"] // 수표 효력 무효
-let number_punctuation_valid = [":", "-", ".", "·", "⠲", "⠐"] // 수표 효력 유효
+let number_punctuation_valid = [":", "-", ".", "·", "⠲", "⠐","⠤"] // 수표 효력 유효
 
 
 func translateNumber(text: String) -> String{
